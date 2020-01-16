b3766667260113311b4d6b824c7ef258d3fee81f
unknown <grantludwig97@gmail.com>
2020-01-15 20:23:46 -0800

    debugging

--- hw2.cpp
+++ hw2.cpp
@@ -117,6 +117,7 @@ int main() {
 
     int check = 1;
     for (int elem: prefix)
+        cout << "Run" << endl;
         if (elem != check++) {
             cout << "FAILED RESULT at " << check-1;
             break;
