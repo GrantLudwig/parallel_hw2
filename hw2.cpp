30e5f94e5baa027b9ff862e69e5275ae16844c93
unknown <grantludwig97@gmail.com>
2020-01-19 18:46:48 -0800

    testing

--- hw2.cpp
+++ hw2.cpp
@@ -8,8 +8,9 @@
 #include <math.h>
 using namespace std;
 
-const int N = 1<<26;  // FIXME must be power of 2 for now
+//const int N = 1<<26;  // FIXME must be power of 2 for now
 //const int N = 9;
+const int N = 100000000;
 typedef vector<int> Data;
 
 class Heaper {
